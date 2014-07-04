fabb0816990fa41172301752fc938688a25afc93
aircross <c>
2014-07-05 01:29:20 +0800

    change gateway port from 2060 to 80

--- src/luci/model/cbi/wifidog.lua
+++ src/luci/model/cbi/wifidog.lua
@@ -6,7 +6,7 @@ local sys = require "luci.sys"
 local fs = require "nixio.fs"
 local uci = require "luci.model.uci".cursor()
 
---limeng
+--Willing
 local running=(luci.sys.call("pidof wifidog > /dev/null") == 0)
 local button=""
 
@@ -29,7 +29,19 @@ if fs.access("/usr/bin/wifidog") then
 	gateway_interface = s:option(Value,"gateway_interface",translate("lan"),translate("br-lan"))
 	gateway_eninterface = s:option(Value,"gateway_eninterface",translate("wan"),translate("eth1/eth2"))
 	gateway_hostname = s:option(Value,"gateway_hostname",translate("auth host"),translate("etc:auth.wifias.com"))
-	gateway_httpport = s:option(Value,"gateway_httpport",translate("server prot"),translate("etc:2060"))
+	gateway_httpport = s:option(Value,"gateway_httpport",translate("server prot"),translate("etc:80"))
+	gateway_path = s:option(Value,"gateway_path",translate("url"),translate("/demoToken/"))
+	gateway_connmax = s:option(Value,"HTTPDMaxConn",translate("max user"))
+	client_timeout = s:option(Value,"client_timeout",translate("client timeout"))
+	check_interval = s:option(Value,"check_interval",translate("check interval"))
+	TrustedHostList = s:option(Value,"TrustedHostList",translate("Trusted Host List"),translate("etc:www.wifias.com,user.wifias.com"))
+	TrustedMACList = s:option(Value,"TrustedMACList",translate("Trusted MAC List"),translate("etc:00:22:11:A4:BC:88,00:22:11:A4:BC:89"))
+
+	gatewayID = s:option(Value,"gateway_id",translate("GatewayID"),translate("id（GatewayID）,etc:demoNode"))
+	gateway_interface = s:option(Value,"gateway_interface",translate("lan"),translate("br-lan"))
+	gateway_eninterface = s:option(Value,"gateway_eninterface",translate("wan"),translate("eth1/eth2"))
+	gateway_hostname = s:option(Value,"gateway_hostname",translate("auth host"),translate("etc:auth.wifias.com"))
+	gateway_httpport = s:option(Value,"gateway_httpport",translate("server prot"),translate("etc:80"))
 	gateway_path = s:option(Value,"gateway_path",translate("url"),translate("/demoToken/"))
 	gateway_connmax = s:option(Value,"HTTPDMaxConn",translate("max user"))
 	client_timeout = s:option(Value,"client_timeout",translate("client timeout"))
